dade1dfc6df71653028062cc1f3088b2ea82e3c1
Ryan Meyer <ryan1234@gmail.com>
2015-09-22 21:03:46 -0700

    First commit.

---
+++ Abenity.Api/ClientKeys.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Abenity.Api
+{
+    public class ClientKeys
+    {
+        internal StreamReader PrivateKeyFileStream { get; private set; }
+        internal StreamReader AbenityPublicKeyFileStream { get; private set; }
+
+        /// <summary>
+        /// Your own generated private key.
+        /// </summary>
+        /// <param name="privateKeyPath">File system location to your private key file</param>
+        /// <param name="abenityPublicKeyPath">Location of Abenity's public key file</param>
+        public ClientKeys(string privateKeyPath, string abenityPublicKeyPath)
+        {
+            PrivateKeyFileStream = File.OpenText(privateKeyPath);
+            AbenityPublicKeyFileStream = File.OpenText(abenityPublicKeyPath);
+        }
+    }
+}
